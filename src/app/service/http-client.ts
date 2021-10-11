10755f4457d9d062ef1e71fdd67add1cadb13f70
hcbarros <48363350+hcbarros@users.noreply.github.com>
2021-10-11 18:04:14 -0300

    Update http-client.ts

--- src/app/service/http-client.ts
+++ src/app/service/http-client.ts
@@ -4,9 +4,12 @@ import axios from 'axios';
 
 export class Http {
 
-  static httpClient() {    
+  static httpClient(token) {    
     return axios.create({
-      baseURL: "http://localhost:8080/doadores"
+      baseURL: "http://localhost:8080/doadores",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
     });
   }
 
